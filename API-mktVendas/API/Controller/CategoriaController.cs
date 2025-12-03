4c624258cdcdf14f76fdebe9262176b9df8dd53b
clemersoncaetano <clemersoncaetano346@gmail.com>
2025-12-02 22:43:47 -0300

    categoria

---
+++ API-mktVendas/API/Controller/CategoriaController.cs
@@ -0,0 +1,67 @@
+﻿using API_mktVendas.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriaController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public CategoriaController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Categoria>>> Get()
+    {
+        return await _context.Categorias.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Categoria>> GetById(int id)
+    {
+        var categoria = await _context.Categorias.FindAsync(id);
+
+        if (categoria == null)
+            return NotFound();
+
+        return categoria;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Categoria>> Post(Categoria categoria)
+    {
+        _context.Categorias.Add(categoria);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetById), new { id = categoria.Id }, categoria);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Categoria categoria)
+    {
+        if (id != categoria.Id)
+            return BadRequest();
+
+        _context.Entry(categoria).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var categoria = await _context.Categorias.FindAsync(id);
+
+        if (categoria == null)
+            return NotFound();
+
+        _context.Categorias.Remove(categoria);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
---
+++ API-mktVendas/Domain/Entities/Categoria.cs
@@ -0,0 +1,10 @@
+﻿using projeto_vwndas.Projeto_Vendas_API.Domain.Entities;
+
+public class Categoria
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+
+    // Relacionamento
+    public ICollection<Produto>? Produtos { get; set; }
+}
